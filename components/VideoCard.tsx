92051e8cf88f3d50a675876080b31adfbc9c6649
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2022-07-17 22:51:39 +0100

    Main section video component

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { NextPage } from 'next'
+import Link from 'next/link'
+import Image from 'next/image'
 import { Video } from '../types'
 
 interface IProps {
@@ -8,7 +10,26 @@ interface IProps {
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
   return (
-    <div>VideoCard</div>
+    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+      <div>
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+          <div className="md:w-10 md:h-16 w-10 h-10">
+            <Link href="/">
+              <>
+                <Image
+                  width={62}
+                  height={62}
+                  className="rounded-full"
+                  src={post.postedBy.image}
+                  alt="profile photo"
+                  layout='responsive'
+                />
+              </>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  images: {
+    domains: ['drive.google.com'],
+  }
 }
 
 module.exports = nextConfig
